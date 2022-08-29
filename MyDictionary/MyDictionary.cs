4636f1aaee5995aa18c2990943e9c7a59082fa5a
sametbakar <sametcb.30@gmail.com>
2022-08-29 18:53:14 +0300

    Add project files.

---
+++ MyDictionary/MyDictionary.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyDictionary
+{
+    class MyDictionary<TKey,TValue>
+    {
+        KeyValuePair<TKey, TValue>[] _items;
+        KeyValuePair<TKey, TValue>[] _tempArray;
+
+        public MyDictionary()
+        {
+            _items = new KeyValuePair<TKey, TValue>[0];
+        }
+
+        public void Add(TKey key,TValue value)
+        {
+            if (KeyControl(key))
+            {
+                _tempArray = _items;
+                _items = new KeyValuePair<TKey, TValue>[_items.Length+1];
+                for (int i = 0; i < _tempArray.Length; i++)
+                {
+                    _items[i] = _tempArray[i];
+                }
+                _items[_items.Length - 1] = new KeyValuePair<TKey, TValue>(key,value);
+            }
+            else
+            {
+                Console.WriteLine("Hatalı anahtar girişi");
+            }
+        }
+
+        private bool KeyControl(TKey key)
+        {
+            bool control = true;
+
+            for (int i = 0; i < _items.Length; i++)
+            {
+                if (_items[i].Key.ToString()==key.ToString())
+                {
+                    control = false;
+                }
+            }
+            return control;
+        }
+        public int Count
+        {
+            get { return _items.Length; }
+        }
+        public void Show()
+        {
+            foreach (var item in _items)
+            {
+                Console.WriteLine("Key: "+item.Key+ "   Value: "+item.Value);
+            }
+        }
+    }
+}
---
+++ MyDictionary/Program.cs
@@ -0,0 +1,25 @@
+﻿
+using System.Collections.Generic;
+
+
+namespace MyDictionary
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var myDictionary=new MyDictionary<int, string>();
+            myDictionary.Add(1, "Ali");
+            myDictionary.Add(1, "Ayşe");
+            myDictionary.Add(3, "Fatma");
+            myDictionary.Add(4, "Hüseyin");
+            myDictionary.Add(5, "Serdar");
+            myDictionary.Add(6, "Hakan");
+
+
+            myDictionary.Show();
+            Console.WriteLine(myDictionary.Count);
+
+        }
+    }
+}
